aa738bdc678f07895a4a3cd4d8355fb359935ce1
Josh-1494 <79804411+Josh-1494@users.noreply.github.com>
2022-11-14 00:38:11 -0600

    Creacion de Proyecto --Coneccion con Base de Datos Oracle a Portal Noticias --Creacion de la Ventana Login (Interfaz Grafica)

---
+++ src/Modelos/ConexionDB.java
@@ -0,0 +1,24 @@
+package Modelos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class ConexionDB {
+    Connection conectar = null;
+    String myDB = "jdbc:oracle:thin:@localhost:1521:orcl";//URL DE SQL DEVELOPER
+    String usuario="PORTALNOTICIAS";
+    String password="ADMIN123";
+            
+        public Connection getConneccion(){
+     try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conectar = (Connection) DriverManager.getConnection(myDB,usuario, password);
+           // JOptionPane.showMessageDialog(null, "pudo acceder");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error de Conexion " + ex.getMessage() ,"Error",JOptionPane.ERROR_MESSAGE);
+        } 
+        return conectar;
+    }  
+   
+}
+
